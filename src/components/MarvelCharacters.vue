c41534f949e97fa89cd1aef77a6d33351bfdb3b3
Nicolas Schmidt <nicolasdrivebackup@gmail.com>
2023-09-03 11:38:33 +0200

    characters store added

---
+++ src/components/MarvelCharacters.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h1>Marvel</h1>
+      <ul>
+        <li v-for="character in characters" :key="character.id">
+          {{ character.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+import { computed, onMounted } from 'vue';
+import { useMarvelStore } from '@/store';
+
+export default {
+  name: 'MarvelCharacters',
+  setup() {
+    const marvelStore = useMarvelStore();
+    const characters = computed(() => marvelStore.characters);
+
+    onMounted(() => {
+      marvelStore.fetchCharacters();
+    });
+
+    return {
+      characters,
+    };
+  },
+};
+</script>
---
+++ src/store.js
@@ -0,0 +1,56 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import CryptoJS from 'crypto-js';
+
+const publicKey = 'b81a4267871891036965e4f23678bb3e';
+const privateKey = 'f9d23e623c8c50347303121267767c99f1adb4c3';
+
+const instance = axios.create({
+  baseURL: 'https://gateway.marvel.com/v1/public',
+});
+
+instance.interceptors.request.use(config => {
+  const timestamp = new Date().getTime();
+  const hash = CryptoJS.MD5(timestamp + privateKey + publicKey).toString();
+
+  config.params = {
+    ...config.params,
+    ts: timestamp,
+    apikey: publicKey,
+    hash: hash,
+  };
+
+  return config;
+});
+
+export const useMarvelStore = defineStore('marvel', {
+  state: () => ({
+    characters: [],
+  }),
+  actions: {
+    async fetchCharacters() {
+      try {
+        const response = await instance.get('/characters');
+        this.characters = response.data.data.results;
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    },
+    async fetchCharacterComics(characterId) {
+      try {
+        const response = await instance.get(`/characters/${characterId}/comics`);
+        return response.data.data.results;
+      } catch (error) {
+        console.error('Error fetching character comics:', error);
+      }
+    },
+    async fetchCharacterSeries(characterId) {
+      try {
+        const response = await instance.get(`/characters/${characterId}/series`);
+        return response.data.data.results;
+      } catch (error) {
+        console.error('Error fetching character series:', error);
+      }
+    },
+  },
+});
