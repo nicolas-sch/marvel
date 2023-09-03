aa515beadb78f0794b1c36f6be7953ded085e7e9
Nicolas Schmidt <nicolasdrivebackup@gmail.com>
2023-09-03 11:57:03 +0200

    MarvelCharcters component created

--- src/components/MarvelCharacters.vue
+++ src/components/MarvelCharacters.vue
@@ -1,31 +1,33 @@
 <template>
-    <div>
-      <h1>Marvel</h1>
-      <ul>
-        <li v-for="character in characters" :key="character.id">
-          {{ character.name }}
-        </li>
-      </ul>
+    <div class="character-wrapper">
+      <div class="character-card" v-for="character in characters" :key="character.id">
+        <div class="character-image">
+          <img :src="character.thumbnail.path + '.' + character.thumbnail.extension" alt="Character Image" />
+        </div>
+        <div class="character-name">{{ character.name }}</div>
+      </div>
     </div>
   </template>
   
   <script>
-import { computed, onMounted } from 'vue';
-import { useMarvelStore } from '@/store';
-
-export default {
-  name: 'MarvelCharacters',
-  setup() {
-    const marvelStore = useMarvelStore();
-    const characters = computed(() => marvelStore.characters);
-
-    onMounted(() => {
-      marvelStore.fetchCharacters();
-    });
-
-    return {
-      characters,
-    };
-  },
-};
-</script>
+  import { computed, onMounted } from 'vue';
+  import { useMarvelStore } from '@/store';
+  import '@/styles/MarvelCharacters.scss';
+  
+  export default {
+    name: 'MarvelCharacters',
+    setup() {
+      const marvelStore = useMarvelStore();
+      const characters = computed(() => marvelStore.characters);
+  
+      onMounted(() => {
+        marvelStore.fetchCharacters();
+      });
+  
+      return {
+        characters,
+      };
+    },
+  };
+  </script>
+  
